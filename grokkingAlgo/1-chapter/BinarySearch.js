d14a75d8d78f58ffc7a86ea320b04f16e4ef719b
Дукенбай Урзия Әбдіхалыққызы <Urziya.Dukenbay@kaspi.kz>
2022-03-15 19:16:53 +0600

    first commit

---
+++ grokkingAlgo/1-chapter/BinarySearch.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+ var binary_search = function(nums) {
+    
+
+    return null;
+};
+
+console.log(binary_search([2,7,9,11,15]));
